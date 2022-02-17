4322a44aa90623303c546f02ed9a6753a6e63680
oah1006 <bnhao10062001@gmail.com>
2022-02-17 16:45:37 +0700

    flutter: study dart basic(variable, string, array)

--- lib/main.dart
+++ lib/main.dart
@@ -3,66 +3,36 @@ import 'dart:math';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
+
+String s1 = 'How are you?';
+String s2 = ' I\'m fine';
+String content = '$s1 - $s2';
+List<int> numbers = [1, 4, 6, 8];
+List<String> stringNumbers = numbers.map((number) {
+  return 'value = $number';
+}).toList();
+
 void main() {
-  runApp(const MyApp());
+  // for (int i = 0; i < numbers.length; i++) {
+  //   print(numbers[i]);
+  // }
+
+  // numbers.forEach((number) {
+  //   print(number);
+  // });
+
+  stringNumbers.forEach((str) {
+    print(str);
+  });
+
+  runApp(
+    Center(
+      child: Text(
+        '$stringNumbers',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 40),
+      )
+    )
+  );
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile (
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      itemBuilder: (context, i) {
-        if (i.isOdd) {
-          return const Divider();
-        }
-
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup OaHang'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Startup OaHang',
-      home: RandomWords(),
-    );
-  }
-}
